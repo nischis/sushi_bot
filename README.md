f77b5814f884ed5eef28a239c9cbb35059e01632
Tosachi <tscmgsn.42@gmail.com>
2018-06-20 00:07:30 +0900

    separate config

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # sushi bot.
 
 This is Twitter bot.
-I will reply, if my Twitter follower tweet "お寿司ガチャ".
+I will reply, if my twitter follower tweet "お寿司ガチャ".
 
 This code is refactoring of Bot_of_Twitter.
---
+++ config.py
@@ -0,0 +1,15 @@
+import tweepy
+
+def auth():
+  
+  # https://apps.twitter.com/ にアクセスし各種キーを取得する
+  # " "内には取得した各種キーを代入する
+  CK = "CONSUMER_KEY"
+  CS = "CONSUMER_SECRET"
+  AT = "ACCESS_TOKEN"
+  AS = "ACCESS_SECRET"
+
+  auth = tweepy.OAuthHandler(CK, CS)
+  auth.set_access_token(AT, AS)
+
+  return auth
